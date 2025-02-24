f84a868ffd8a2b937334d7a066a699b662ab2781
DhanasekarA-18 <dhanasekard848@gmail.com>
2025-02-24 07:59:36 +0530

    build-error-fixed

--- src/app/utils/helper.js
+++ src/app/utils/helper.js
@@ -1,12 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
 
 const getUserId = () => {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
   const USER_ID_KEY = "userId";
   let userId = sessionStorage.getItem(USER_ID_KEY);
+
   if (!userId) {
     userId = uuidv4();
     sessionStorage.setItem(USER_ID_KEY, userId);
   }
+
   return userId;
 };
 
